2a72dfa6c21244573ef71a59af8c68c638dd533f
taku <test@test.com>
2026-05-12 17:52:55 +0900

    fix contact properly

---
+++ app/contact/page.tsx
@@ -0,0 +1,13 @@
+export default function Contact() {
+  return (
+    <main>
+      <h1>お問い合わせ</h1>
+
+      <p>
+        ご意見・お問い合わせは下記メールまでお願いいたします。
+      </p>
+
+      <p>Email: your-email@example.com</p>
+    </main>
+  );
+}
