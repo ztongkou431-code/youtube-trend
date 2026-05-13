9fac6c4740cd8fda405fa0f6d6b9ccf3b0c99a81
taku <test@test.com>
2026-05-13 10:58:49 +0900

    step2 show titles

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,19 @@
-export default function Home() {
+import { getTrendingVideos } from "../lib/youtube";
+
+export default async function Home() {
+  const videos = await getTrendingVideos();
+
   return (
     <main>
-      <h1>OK</h1>
-      <p>動作確認ページ</p>
+      <h1>🔥 YouTubeトレンド</h1>
+
+      <ul>
+        {videos.map((video: any) => (
+          <li key={video.id}>
+            {video.snippet.title}
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
