8d15dc3505b413d9ef11a137210fa5f91c3ef79d
taku <test@test.com>
2026-05-13 13:34:11 +0900

    fix css load

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
+import "./globals.css";
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html>
+    <html lang="ja">
       <body>{children}</body>
     </html>
   );
