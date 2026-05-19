b2151eefeefb80e34da2a281323b782b4f49cf41
taku <test@test.com>
2026-05-19 11:14:35 +0900

    add ads.txt

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -1,8 +1,20 @@
 export default function Privacy() {
   return (
-    <main>
+    <div style={{ padding: "20px" }}>
       <h1>プライバシーポリシー</h1>
-      <p>このサイトでは広告のためにCookieを使用します。</p>
-    </main>
+
+      <p>
+        当サイトでは、第三者配信の広告サービス（Google AdSense）を利用しています。
+      </p>
+
+      <p>
+        このような広告配信事業者は、ユーザーの興味に応じた商品やサービスの広告を表示するため、
+        Cookie（クッキー）を使用することがあります。
+      </p>
+
+      <p>
+        Cookieを無効にする設定やGoogle AdSenseに関する詳細はGoogleポリシーをご確認ください。
+      </p>
+    </div>
   );
 }
